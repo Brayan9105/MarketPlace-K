4f5351f78e49337b087c194fec0afa73e1cd7c66
Brayan Lopez <brayan9105@hotmail.com>
2020-05-05 12:54:54 -0500

    Added colums for recents products and user to the admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,25 +3,26 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc { I18n.t("active_admin.dashboard") } do
 
+    columns do
+      column do
+        panel "Recent Products" do
+          ul do
+            Product.last(5).each do |product|
+              li link_to(product.name, admin_product_path(product))
+            end
+          end
+        end
+      end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Recent Users" do
+          ul do
+            User.last(5).each do |user|
+              li link_to(user.email, admin_user_path(user))
+            end
+          end
+        end
+      end
+    end
   end # content
 end
