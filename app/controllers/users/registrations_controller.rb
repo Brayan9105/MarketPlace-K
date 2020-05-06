d6cdfae685f56a3b24291a5eaf8c3f9208701f90
Brayan Lopez <brayan9105@hotmail.com>
2020-05-06 11:37:03 -0500

    Added a method to avoid access to other users edit profile this will redirect to the user profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :same_user!
 
   # GET /resource/sign_up
   def new
@@ -61,4 +62,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def same_user!
+    user = User.find(params[:format])
+    if current_user.id != user.id
+      redirect_to user
+    end
+  end
 end
