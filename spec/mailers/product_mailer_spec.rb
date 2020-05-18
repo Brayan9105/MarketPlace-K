8e3fd3563534320fcc51d058e56728e9ca337805
Brayan Lopez <brayan9105@hotmail.com>
2020-05-18 18:10:52 -0500

    Added rspec test for mail

--- spec/mailers/product_mailer_spec.rb
+++ spec/mailers/product_mailer_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe ProductMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "mail" do
+    let(:user) {User.create(email: 'test@test.com', password: '12345678')}
+    let(:product) {Product.create(name: 'product test', user: user)}
+    let(:mail) { ProductMailer.product_published(product).deliver }
+
+    context "When a product is published" do
+      it "renders the headers" do
+        expect(mail.subject).to eq("A new product have been published")
+        expect(mail.to).to eq(["test@test.com"])
+        expect(mail.from).to eq(["brayanlopez9105@gmail.com"])
+      end
+    end
+  end
 end
